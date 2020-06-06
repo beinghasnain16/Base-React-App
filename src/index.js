9235724ff9e229f11c957847947446ea3ef01335
Hasnain Ali <beinghasnain16@gmail.com>
2020-06-06 23:33:50 +0500

    learning react

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>App  name="Hasnain Ali" age={17} /></div>,
   document.getElementById('root')
 );
 
